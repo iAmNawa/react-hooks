c3512b107f30bbdc6686c9bd83e99624ffe8be1f
Paul Borawski <paulborawski14@gmail.com>
2019-03-31 12:49:14 -0700

    refactor that accomplishes the same thing as the previous commit using useState and useEffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class ResourceList extends Component {
-  state = { resources: [] };
+const ResourceList = ({ resource }) => {
+  // Initialized state - [], resources has access, and setResources can change it
+  const [resources, setResources] = useState([]);
 
-  async componentDidMount() {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
-    this.setState({ resources: response.data });
+  const fetchResource = async (resource) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+    // setState
+    setResources(response.data);
   }
 
-  async componentDidUpdate(prevProps) {
-    if (prevProps.resource !== this.props.resource) {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
-    this.setState({ resources: response.data });
-    }
-  }
+  // ComponentDidMount and ComponentDidUpdate called here
+  useEffect(() => {
+    fetchResource(resource);
+    // Arrow function above is not called if resource below is the same
+  }, [resource])
 
-  render() {
-    return (
-      <div>{this.state.resources.length}</div>
-    );
-  }
+  return (
+    <div>{resources.length}</div>
+  );
 }
 
 export default ResourceList;
