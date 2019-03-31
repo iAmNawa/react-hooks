a3b9c6d881e71b1e9499d5f5c675ff1ba036619c
Paul Borawski <paulborawski14@gmail.com>
2019-03-31 13:08:40 -0700

    for async or promises you can use an IIFE inside useEffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -5,15 +5,13 @@ const ResourceList = ({ resource }) => {
   // Initialized state - [], resources has access, and setResources can change it
   const [resources, setResources] = useState([]);
 
-  const fetchResource = async (resource) => {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
-    // setState
-    setResources(response.data);
-  }
-
   // ComponentDidMount and ComponentDidUpdate called here
   useEffect(() => {
-    fetchResource(resource);
+    (async (resource) => {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+      // setState
+      setResources(response.data);
+    })(resource)
     // Arrow function above is not called if resource below is the same
   }, [resource]);
 
