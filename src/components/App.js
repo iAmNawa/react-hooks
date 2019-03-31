843b0d6d1886e973328e286c53cf38adb9be7acc
Paul Borawski <paulborawski14@gmail.com>
2019-03-31 08:44:39 -0700

    simple state change onClick

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
 class App extends React.Component {
+  state = { resource: 'posts' };
   render() {
     return (
-      <div>App</div>
+      <div>
+        <div>
+          <button onClick={() => this.setState({ resource: 'posts' })}>Posts</button>
+          <button onClick={() => this.setState({ resource: 'todos' })}>Todos</button>
+        </div>
+        {this.state.resource}
+      </div>
     );
   }
 }
